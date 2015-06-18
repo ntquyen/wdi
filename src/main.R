b8a7d55eb9bbc6bcf1e87a899e45cb594a8e7eef
ntquyen <nguyen.tuan.quyen.it@gmail.com>
2015-06-18 07:05:31 +0700

    Refactor code

--- src/main.R
+++ src/main.R
@@ -11,16 +11,28 @@ end.year = 2014
 eap.countries <- wdi.countries %>% filter(country %in% eap.country.names)
 
 eap.countries.ny.gdp.mktp.cd <- getCountriesByIndicator(eap.countries$iso2c, ny.gdp.mktp.cd, 1990, 2014)
-g.NY.GDP.MKTP.CD <- plot.NY.GDP.MKTP.CD(eap.countries.ny.gdp.mktp.cd, "graphics/GDP.png", 1990, 2014)
+g.NY.GDP.MKTP.CD <- plot.wdi(eap.countries.ny.gdp.mktp.cd, 'graphics/GDP.png', 1990, 2014, 
+                             "year", ny.gdp.mktp.cd, "country",
+                             "GDP (current US$)", "Year", "GDP (billion $)",
+                             scale.y = 1e+9)
 
 eap.countries.ny.gdp.mktp.kd.zg <- getCountriesByIndicator(eap.countries$iso2c, ny.gdp.mktp.kd.zg, 1990, 2014)
-g.NY.GDP.MKTP.KD.ZG <- plot.NY.GDP.MKTP.KD.ZG(eap.countries.ny.gdp.mktp.kd.zg, "graphics/GDP_growth.png", 1990, 2014)
+g.NY.GDP.MKTP.KD.ZG <- plot.wdi(eap.countries.ny.gdp.mktp.kd.zg, 'graphics/GDP_growth.png', 1990, 2014, 
+                                "year", ny.gdp.mktp.kd.zg, "country",
+                                "GDP growth (annual %)", "Year", "Growth (%)",
+                                scale.y = 1)
 
 eap.countries.sl.gdp.pcap.em.kd <- getCountriesByIndicator(eap.countries$iso2c, sl.gdp.pcap.em.kd, 1990, 2014)
-g.SL.GDP.PCAP.EM.KD <- plot.SL.GDP.PCAP.EM.KD(eap.countries.sl.gdp.pcap.em.kd, "graphics/GDP_person.png", 1990, 2014)
+g.SL.GDP.PCAP.EM.KD <- plot.wdi(eap.countries.sl.gdp.pcap.em.kd, 'graphics/GDP_person.png', 1990, 2014, 
+                                "year", sl.gdp.pcap.em.kd, "country",
+                                "GDP per person employed ($)", "Year", "GDP ($)",
+                                scale.y = 1)
 
 eap.countries.ny.gdp.pcap.cd <- getCountriesByIndicator(eap.countries$iso2c, ny.gdp.pcap.cd, 1990, 2014)
-g.NY.GDP.PCAP.CD <- plot.NY.GDP.PCAP.CD(eap.countries.ny.gdp.pcap.cd, "graphics/GDP_per_capita.png", 1990, 2014)
+g.NY.GDP.PCAP.CD <- plot.wdi(eap.countries.ny.gdp.pcap.cd, 'graphics/GDP_per_capita.png', 1990, 2014, 
+                             "year", ny.gdp.pcap.cd, "country",
+                             "GDP per capita ($)", "Year", "GDP ($)",
+                             scale.y = 1)
 
 g <- arrangeGrob(g.NY.GDP.MKTP.CD, g.NY.GDP.MKTP.KD.ZG, g.SL.GDP.PCAP.EM.KD, g.NY.GDP.PCAP.CD, ncol = 2)
 ggsave(file = "./graphics/GDPs.png", g)
--- src/plot.R
+++ src/plot.R
@@ -7,20 +7,28 @@ getGgplotTheme <- function() {
         legend.title = element_blank()))
 }
 
+scaleY <- function(scale.y) {
+  return (scale_y_continuous(label = function(value) {
+    return (value/scale.y)
+    }))
+}
+
 exportPNG <- function(g, file) {
   file.path <- paste(getwd(), file, sep = "/")
   print(file.path)
   ggsave(file = file.path)
 }
 
-
-plot.NY.GDP.MKTP.CD <- function(data, file = '', from.year, to.year) {
-  g <- ggplot(data, aes(year, NY.GDP.MKTP.CD, group = country, color = country))
+plot.wdi <- function(data, file = '', 
+                     from.year, to.year, 
+                     x, y, group,
+                     title, xlab, ylab,
+                     scale.y = 1) {
+  
+  g <- ggplot(data, aes_string(x = x, y = y, group = group, color = group))
   g <- g + geom_line(size = 1) + 
-    labs(title = "GDP (current US$)", x = "Year", y = "GDP (billion US$)") +
-    scale_y_continuous(label = function(value) {
-      return (value/1e+9)
-    }) + getGgplotTheme() +
+    labs(title = title, x = xlab, y = ylab) +
+    scaleY(scale.y) + getGgplotTheme() +
     xlim(from.year, to.year)
   
   if(nchar(file) > 0) {
@@ -29,48 +37,3 @@ plot.NY.GDP.MKTP.CD <- function(data, file = '', from.year, to.year) {
   
   return (g)
 }
-
-plot.NY.GDP.MKTP.KD.ZG <- function(data, file = "", from.year, to.year) {
-  g <- ggplot(data, aes(year, NY.GDP.MKTP.KD.ZG, group = country, color = country))
-  g <- g + geom_line(size = 1) + 
-    labs(title = "GDP growth (annual %)", x = "Year", y = "Growth (%)") +
-    getGgplotTheme() +
-    xlim(from.year, to.year)
-  
-  if(nchar(file) > 0) {
-    exportPNG(g, file)
-  }
-  
-  return (g)
-}
-
-
-plot.SL.GDP.PCAP.EM.KD <- function(data, file = "", from.year, to.year) {
-  g <- ggplot(data, aes(year, SL.GDP.PCAP.EM.KD, group = country, color = country))
-  g <- g + geom_line(size = 1) + 
-    labs(title = "GDP per person employed ($)", x = "Year", y = "GDP") +
-    getGgplotTheme() + 
-    xlim(from.year, to.year)
-  
-  if(nchar(file) > 0) {
-    exportPNG(g, file)
-  }
-  
-  return (g)
-}
-
-
-plot.NY.GDP.PCAP.CD <- function(data, file = "", from.year, to.year) {
-  g <- ggplot(data, aes(year, NY.GDP.PCAP.CD, group = country, color = country))
-  g <- g + geom_line(size = 1) + 
-    labs(title = "GDP per capita ($)", x = "Year", y = "GDP ($)") +
-    getGgplotTheme() + 
-    xlim(from.year, to.year)
-  
-  if(nchar(file) > 0) {
-    exportPNG(g, file)
-  }
-  
-  return (g)
-}
-
